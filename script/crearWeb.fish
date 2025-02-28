66b1de350e6cf54ca893726d82cd4875ec7080c7
Nicolás Cualangelo <ncualangelo@gmail.com>
2025-02-28 16:36:59 -0300

    Eliminando el arvhivo temporal del menu en index

--- script/crearWeb.fish
+++ script/crearWeb.fish
@@ -1,28 +1,43 @@
 #!/usr/bin/fish
 
+# Rutas de archivos y directorios
 set exportar "../index.html"
 set consolasDir "../consolas"
 set aliasDir "../alias"
+set alias_consola "$aliasDir/alias_consolas.txt"
 
 
 function titulo
-  set -l alias_consola "$aliasDir/alias_consolas.txt"
   set -l consola (basename $argv[1])
-  set -l resultado (awk -F '=' -v consola="$consola" '$1 == consola { print $2 }' $alias_consola)
-  echo "$resultado"
+  awk -F '=' -v consola="$consola" '$1 == consola { print $2 }' $alias_consola
 end
 
-echo "<!DOCTYPE html>
+# Estructura inicial del archivo HTML
+echo '<!DOCTYPE html>
 <html>
 <head>
   <title>🔥 Listado de juegos BOM 🎮️</title>
-  <link rel=\"stylesheet\" href=\"style.css\">
-  <link rel=\"icon\" href=\"data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🎮️</text></svg>\">
+  <link rel="stylesheet" href="style.css">
+  <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🎮️</text></svg>">
 </head>
 <body>
-<h1 id=\"menu\">🔥 Listado de Juegos BOM 🎮️ </h1>" > $exportar
-echo "<nav>" > nav.html
+<h1 id="menu">🔥 Listado de Juegos BOM 🎮️ </h1>
+<nav>' > $exportar
 
+set menu ""
+
+for archivo in (find $consolasDir -type f -name '*.txt' | sort -f -b)
+
+	set numero (wc -l < $archivo)
+	set consola (titulo $archivo )
+  set menu "$menu<a href=\"#$consola\">$consola ($numero)</a>"
+
+end
+
+echo "$menu" >> $exportar
+echo "</nav>" >> $exportar
+
+# Recorrer los archivos de consolas y generar el contenido
 for archivo in (find $consolasDir -type f -name '*.txt' | sort -f -b)
 
 	set numero (wc -l < $archivo)
@@ -34,13 +49,7 @@ for archivo in (find $consolasDir -type f -name '*.txt' | sort -f -b)
 		echo "<div class=\"juego\"><span>$juego</span><span>$consola</span></div>" >> $exportar
 	end
 	echo "</section>" >> $exportar
-	echo "<a href=\"#$consola\">$consola ($numero)</a>" >> nav.html
 end
 
-echo "</nav>" >> nav.html
-set menu (cat nav.html)
-sed -i "9a$menu" $exportar
 echo "</body>
 </html>" >> $exportar
-
-rm nav.html
